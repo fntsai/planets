ccfbe65e122d95ebbb8660880953e14ef5c0cd60
funien tsai <fntsai@u.northwestern.edu>
2016-11-05 00:21:45 -0500

    ADD add re-run MZM run2 to make sure Bam is fine

---
+++ bash/161104.1_alignment.sh
@@ -0,0 +1,14 @@
+/projects/p20742/tools/buildPipelineScripts.draft.pl \
+-o /projects/b1038/Rheumatology/20160725-Bim_Kid-Macs/output \
+-bs /projects/b1038/Rheumatology/20160725-Bim_Kid-Macs/160627_NB501488_0013_AH3KNWBGXY_MZM2 \
+-g mm10 \
+-t RNA \
+-buildBcl2fq 1 \
+-runBcl2fq 1 \
+-runTrim 1 \
+-buildAlign 1 \
+-runAlign 1 \
+-uploadASHtracks 0 \
+-buildEdgeR 0 \
+-runEdgeR 0 \
+>& /projects/b1038/Rheumatology/20160725-Bim_Kid-Macs/output/MMMMZM_output.log &
