46028056313a4c52ec97fa99c9f7beb54dbcfbe0
funien tsai <fntsai@u.northwestern.edu>
2016-11-06 12:57:56 -0600

    ADD add MZM1 alignment re-run…

---
+++ bash/161106_MZM1.alignment.sh
@@ -0,0 +1,14 @@
+/projects/p20742/tools/buildPipelineScripts.draft.pl \
+-o /projects/b1038/Rheumatology/20160725-Bim_Kid-Macs/output_1 \
+-bs /projects/b1038/Rheumatology/20160725-Bim_Kid-Macs/160624_NB501488_0012_AH3HMHBGXY_MZM1 \
+-g mm10 \
+-t RNA \
+-buildBcl2fq 1 \
+-runBcl2fq 1 \
+-runTrim 1 \
+-buildAlign 1 \
+-runAlign 1 \
+-uploadASHtracks 0 \
+-buildEdgeR 0 \
+-runEdgeR 0 \
+>& /projects/b1038/Rheumatology/20160725-Bim_Kid-Macs/output_1/MZM1_output.log &
