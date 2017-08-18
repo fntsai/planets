265d4bf81967dbd49db1bff1a8f428c6c101eeb3
funien tsai <fntsai@u.northwestern.edu>
2017-08-17 23:41:09 -0500

    ADD add R to calculate the qvalues

---
+++ R/20170817 calculating qvalue.R
@@ -0,0 +1,51 @@
+#go to file from your folder
+setwd("/Users/FuNien/Documents/2016_RNA_seq/20170415_POP3/M17-24/FDR")
+#read table
+table.name <- read.table(file="3. noM22.log.trimmed.M17-M24.htseq.normCounts.txt", header = T, sep ="")
+View(table.name)
+
+#pvalue calculation
+p.values <- vector(mode = "numeric", length = nrow(table.name))
+
+for (i in 1:nrow(table.name))
+{
+  ttest=t.test(table.name[i,1:4],table.name[i,5:7])
+  names(ttest)
+  p.values[i]<-ttest$p.value
+}
+
+table.name$pvalues <-p.values
+#qvalue calculation
+
+q <- p.adjust(table.name$pvalues, "bonferroni")
+
+# BGN TEST
+#length(table.name$pvalues)
+
+#data <- table.name$pvalues
+#data <- sort(data)
+#min(data)
+
+#p.adjust(c(0.01, 0.02, 0.03, 0.4998, 1), "hochberg")
+
+              #, method = p.adjust.methods
+               
+#, n= nrow(table.name)
+              #)
+
+#(table.name$pvalues)
+
+# END TEST
+table.name$qvalues <-q
+#calculating logFC
+
+WT.mean <- rowMeans(table.name[,1:4])
+POP3.mean <- rowMeans(table.name[,5:7])
+table.name$WT.mean <- WT.mean
+table.name$POP3.mean <- POP3.mean
+
+
+logFC <- table.name$POP3.mean - table.name$WT.mean
+table.name$logFC <- logFC 
+#export file
+write.csv(table.name, file = "DSSMP.csv")
